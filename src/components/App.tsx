f3aea0407bcd4c6ffff2bf7e35d71842dc9e3830
Bill Sorensen <baldgoth@gmail.com>
2020-04-19 16:11:14 -0600

    Add app styling, version, and attribution

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { version } from '../constants/application';
 import Dots from './Dots';
 import Section from './Section';
-import './App.css';
+import styles from './App.module.css';
 
 const App: React.FC = () => {
   const handleClick = () => {
@@ -10,10 +11,19 @@ const App: React.FC = () => {
 
   return (
     <div className="container-fluid">
-      <h1>Ex Oblivione</h1>
+      <h1>
+        Ex Oblivione <span className={styles.version}> v{version}</span>
+      </h1>
       <Section header="Character">
         <Dots level={3} max={4} onClick={handleClick} />
       </Section>
+      <div className={`row ${styles.attribution}`}>
+        <div className="col-sm-12">
+          Some terms are copyrighted by or registered trademarks of White Wolf
+          Entertainment. This tool is unofficial, and the author is not
+          affiliated with the companies mentioned here.
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/constants/application.ts
@@ -0,0 +1,3 @@
+// General application constants.
+export const version = '0.0.1';
+export const docUrl = 'https://github.com/TrueWill/ex-oblivione';
